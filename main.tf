7b8329ac6f97b825912354498f51fba47ebf9331
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2023-10-23 16:10:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider aws{
   region ="ap-south-1"
- 
+  access_key="AKIAXBL5DJOOQ6OSIEIQ"
+  secret_key = "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"
 }
 resource aws_vpc "VPC"{
   cidr_block="172.0.0.0/24"
@@ -27,10 +28,13 @@ resource aws_route_table_association "ARTA"{
     subnet_id=aws_subnet.SUB1.id
     route_table_id=aws_route_table.RT.id
     }
-resource aws_instance "INST"{
+resource aws_instance "DEMO"{
     instance_type ="t2.micro"
     ami="ami-099b3d23e336c2e83"
     key_name="ansible-jenkins"
     subnet_id=aws_subnet.SUB1.id
     associate_public_ip_address= true
+    availability_zone ="ap-south-1b"
+    
+
 }
