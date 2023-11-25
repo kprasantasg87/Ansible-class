9d9e0166ee9f78fa8c90722aec4f4ccda4631d1d
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2023-11-25 17:04:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider aws {
     region="ap-south-1"
-    access_key="AKIAXBL5DJOOYOWJCRXP"
-    secret_key="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"
+    access_key="AKIAXBL5DJOOYVZTYYWH"
+    secret_key="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"
 }
 
 
